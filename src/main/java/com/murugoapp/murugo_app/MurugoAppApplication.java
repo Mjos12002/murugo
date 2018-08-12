879752e56c09000406448a22c135b718ac907c42
Joseph Manzi <mjos1200@gmail.com>
2018-08-12 12:03:00 +0200

    First commit

---
+++ src/main/java/com/murugoapp/murugo_app/MurugoAppApplication.java
@@ -0,0 +1,12 @@
+package com.murugoapp.murugo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MurugoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MurugoAppApplication.class, args);
+	}
+}
